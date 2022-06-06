e8ace9db2f52796bb04edceff82eb1284c6c1be2
urmilshroff <urmilshroff@gmail.com>
2022-06-06 16:14:47 +0530

    feat: create colors class

---
+++ lib/colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class CrunchColors {
+  static const primaryBlue = Color(0xff007bff);
+  static const lightBlue = Color(0xff4394eC);
+  static const darkBlue = Color(0xff0150C5);
+
+  static const secondary = Color(0xfff1f9ff);
+  static const background = Color(0xfff9f9f9);
+
+  static const lightGrey = Color(0xfff2f2f2);
+  static const grey = Color(0xff8c8c8c);
+  static const darkGrey = Color(0xFF616161);
+  static const superDarkGrey = Color(0xff333431);
+
+  static const error = Color(0xfffa5a5a);
+  static const success = Color(0xFF43A047);
+}
