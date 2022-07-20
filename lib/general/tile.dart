eff52fc0afacf6a7045bb7555ab189c1c78346b0
urmilshroff <urmilshroff@gmail.com>
2022-07-20 13:54:22 +0530

    refactor: rewrite tile widget

--- lib/general/tile.dart
+++ lib/general/tile.dart
@@ -23,34 +23,58 @@ class CrunchTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 0.6.h),
-      decoration: BoxDecoration(
-        color: CrunchColors.superLightWhite,
-        borderRadius: CrunchDimensions.roundBorderRadius,
-      ),
-      child: ListTile(
-        title: title != null
-            ? Flexible(
-                child: CrunchText(
-                  title!,
-                  style: CrunchTextStyles.tileTitleBlack,
-                  maxLines: 1,
-                ),
-              )
-            : null,
-        subtitle: subTitle != null
-            ? CrunchText(
-                subTitle!,
-                style: CrunchTextStyles.subTitleGrey,
-              )
-            : null,
-        leading: leading,
-        trailing: trailing,
-        onTap: () {
-          unawaited(HapticFeedback.mediumImpact());
-          onTap!();
-        },
+    return GestureDetector(
+      onTap: onTap != null
+          ? () {
+              unawaited(HapticFeedback.mediumImpact());
+              onTap!();
+            }
+          : null,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 0.6.h),
+        padding: EdgeInsets.all(CrunchDimensions.horizontalPadding / 2),
+        decoration: BoxDecoration(
+          color: CrunchColors.superLightWhite,
+          borderRadius: CrunchDimensions.roundBorderRadius,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: Row(
+                children: [
+                  leading ?? Container(),
+                  SizedBox(width: CrunchDimensions.horizontalPadding / 2),
+                  Flexible(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Visibility(
+                          visible: title != null,
+                          child: CrunchText(
+                            title!,
+                            style: CrunchTextStyles.tileTitleBlack,
+                            maxLines: 1,
+                          ),
+                        ),
+                        Visibility(
+                          visible: subTitle != null,
+                          child: CrunchText(
+                            subTitle!,
+                            style: CrunchTextStyles.subTitleGrey,
+                            maxLines: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            trailing ?? Container(),
+          ],
+        ),
       ),
     );
   }
