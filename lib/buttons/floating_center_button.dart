5afd3218f7021361433edfd5d0efa3ec81f75c2a
urmilshroff <urmilshroff@gmail.com>
2022-07-29 18:32:24 +0530

    feat: add shimmer border

--- lib/buttons/floating_center_button.dart
+++ lib/buttons/floating_center_button.dart
@@ -1,18 +1,17 @@
 import 'package:crunch_kit/crunch_kit.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:sizer/sizer.dart';
 
 class CrunchFloatingCenterButton extends StatelessWidget {
   final VoidCallback onPressed;
   final Widget child;
-  final Color? color;
 
   const CrunchFloatingCenterButton({
     Key? key,
     required this.onPressed,
     required this.child,
-    this.color,
   }) : super(key: key);
 
   @override
@@ -23,23 +22,45 @@ class CrunchFloatingCenterButton extends StatelessWidget {
         FocusManager.instance.primaryFocus?.unfocus();
         onPressed();
       },
-      child: SizedBox(
-        height: 16.w,
-        width: 16.w,
-        child: Neumorphic(
-          style: NeumorphicStyle(
-            color: color ?? CrunchColors.payCrunchBlue,
-            shape: NeumorphicShape.flat,
-            lightSource: LightSource.top,
-            boxShape: NeumorphicBoxShape.roundRect(
-              CrunchDimensions.circleBorderRadius,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox(
+            height: 18.w,
+            width: 18.w,
+            child: Shimmer.fromColors(
+              baseColor: CrunchColors.superLightWhite,
+              highlightColor: CrunchColors.silver,
+              period: const Duration(seconds: 2),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: CrunchColors.superLightWhite,
+                  borderRadius: CrunchDimensions.circleBorderRadius,
+                ),
+              ),
             ),
           ),
-          child: Padding(
-            padding: EdgeInsets.all(CrunchDimensions.horizontalPadding * 0.7),
-            child: child,
+          SizedBox(
+            height: 15.w,
+            width: 15.w,
+            child: Neumorphic(
+              style: NeumorphicStyle(
+                color: CrunchColors.payCrunchBlue,
+                shape: NeumorphicShape.flat,
+                lightSource: LightSource.top,
+                boxShape: NeumorphicBoxShape.roundRect(
+                  CrunchDimensions.circleBorderRadius,
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(
+                  CrunchDimensions.horizontalPadding * 0.7,
+                ),
+                child: child,
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
