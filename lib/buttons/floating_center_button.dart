2c2a59589b682ff599bef462983ab2c01f3b41a4
urmilshroff <urmilshroff@gmail.com>
2022-08-17 16:13:39 +0530

    feat: update fab

--- lib/buttons/floating_center_button.dart
+++ lib/buttons/floating_center_button.dart
@@ -1,17 +1,16 @@
 import 'package:crunch_kit/crunch_kit.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:lottie/lottie.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:sizer/sizer.dart';
 
 class CrunchFloatingCenterButton extends StatelessWidget {
   final VoidCallback onPressed;
-  final Widget child;
 
   const CrunchFloatingCenterButton({
     Key? key,
     required this.onPressed,
-    required this.child,
   }) : super(key: key);
 
   @override
@@ -26,38 +25,30 @@ class CrunchFloatingCenterButton extends StatelessWidget {
         alignment: Alignment.center,
         children: [
           SizedBox(
-            height: 18.w,
-            width: 18.w,
+            height: 15.5.w,
+            width: 15.5.w,
             child: Shimmer.fromColors(
-              baseColor: CrunchColors.superLightWhite,
-              highlightColor: CrunchColors.silver,
+              baseColor: CrunchColors.payCrunchBlue,
+              highlightColor: CrunchColors.superLightBlue,
               period: const Duration(seconds: 2),
               child: Container(
                 decoration: BoxDecoration(
-                  color: CrunchColors.superLightWhite,
+                  color: CrunchColors.payCrunchBlue,
                   borderRadius: CrunchDimensions.circleBorderRadius,
                 ),
               ),
             ),
           ),
-          SizedBox(
-            height: 15.w,
-            width: 15.w,
-            child: Neumorphic(
-              style: NeumorphicStyle(
-                color: CrunchColors.payCrunchBlue,
-                shape: NeumorphicShape.flat,
-                lightSource: LightSource.top,
-                boxShape: NeumorphicBoxShape.roundRect(
-                  CrunchDimensions.circleBorderRadius,
-                ),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(
-                  CrunchDimensions.horizontalPadding * 0.7,
-                ),
-                child: child,
-              ),
+          Container(
+            height: 13.w,
+            width: 13.w,
+            decoration: BoxDecoration(
+              color: CrunchColors.almostWhite,
+              borderRadius: CrunchDimensions.circleBorderRadius,
+            ),
+            child: Lottie.asset(
+              'assets/lottie/qr_scan.json',
+              reverse: true,
             ),
           ),
         ],
