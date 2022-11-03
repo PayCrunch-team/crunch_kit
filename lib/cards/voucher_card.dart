2f5ee10a131f20e34af87893fdc670c7522fffc1
urmilshroff <urmilshroff@gmail.com>
2022-11-03 19:55:41 +0530

    refactor: use row/columns instead of stack

--- lib/cards/voucher_card.dart
+++ lib/cards/voucher_card.dart
@@ -1,5 +1,6 @@
 import 'package:crunch_kit/crunch_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'package:sizer/sizer.dart';
 
@@ -35,60 +36,67 @@ class CrunchVoucherCard extends StatelessWidget {
     return GestureDetector(
       onTap: onPressed != null
           ? () async {
-              // await HapticFeedback.mediumImpact();
+              await HapticFeedback.mediumImpact();
               FocusManager.instance.primaryFocus?.unfocus();
               onPressed!();
             }
           : null,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(
-                vertical: CrunchDimensions.verticalPadding / 2,
-                horizontal: CrunchDimensions.horizontalPadding / 1.5),
-            width: 38.w,
-            decoration: BoxDecoration(
-              color: dominantColor,
-              borderRadius: CrunchDimensions.roundBorderRadius,
-            ),
-            child: Column(
+      child: Container(
+        width: 42.w,
+        height: 36.h,
+        padding: EdgeInsets.symmetric(
+          vertical: CrunchDimensions.verticalPadding / 2,
+          horizontal: CrunchDimensions.horizontalPadding / 2,
+        ),
+        decoration: BoxDecoration(
+          color: dominantColor,
+          borderRadius: CrunchDimensions.roundBorderRadius,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  width: 13.w,
-                  height: 13.w,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: imageProvider,
-                      fit: BoxFit.cover,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 13.w,
+                      height: 13.w,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: CrunchColors.black.withOpacity(0.1),
+                            spreadRadius: 1,
+                            blurRadius: 3,
+                            offset: const Offset(0, 4),
+                          )
+                        ],
+                        borderRadius: CrunchDimensions.circleBorderRadius,
+                      ),
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        spreadRadius: 1,
-                        blurRadius: 3,
-                        offset: const Offset(0, 4),
-                      )
-                    ],
-                    borderRadius: CrunchDimensions.circleBorderRadius,
-                  ),
+                    Image.asset(
+                      'assets/illustrations/stars.png',
+                      width: 22.w,
+                    ),
+                  ],
                 ),
-                SizedBox(height: 2.h),
                 CrunchText(
                   'Up to'.toUpperCase(),
                   style: CrunchTextStyles.subTitleWhite,
                 ),
                 CrunchText(desc, style: CrunchTextStyles.titleWhite),
-                Expanded(child: Container()),
-                SizedBox(height: 2.h),
               ],
             ),
-          ),
-          Positioned(
-            bottom: 1.5.h,
-            child: Container(
+            Container(
               width: 32.w,
               height: 4.h,
               decoration: BoxDecoration(
@@ -116,16 +124,8 @@ class CrunchVoucherCard extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-          Positioned(
-            top: 1.5.h,
-            right: 2.w,
-            child: Image.asset(
-              'assets/illustrations/stars.png',
-              width: 25.w,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
